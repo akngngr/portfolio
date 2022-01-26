5426ea9fd92f1b6cc3fec9d31f6dba88a6063257
Akin <akingungor@yahoo.com>
2022-01-26 17:26:58 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <div className="app">
       
       <div>

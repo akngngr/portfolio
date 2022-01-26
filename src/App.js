5fb70a0ae9628dd7c134893f21b9346a82d97427
Akin <akingungor@yahoo.com>
2022-01-26 13:39:45 -0500

    prefixed router links

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className="app">
       
       <div>
